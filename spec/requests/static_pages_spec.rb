1a92f3292d1a8c9b765dfabe495b4272089dcfeb
jjbachiller <jjbachiller@gmail.com>
2012-09-26 22:10:45 +0200

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,36 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:title_start) { title_start = 'Ruby on Rails Tutorial Sample App' }
-
   subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector 'h1', text: 'Sample App' }
-    it { should have_selector 'title', text: full_title("") }
+    it { should have_selector 'h1',        text: 'Sample App' }
+    it { should have_selector 'title',     text: full_title("") }
     it { should_not have_selector 'title', text: ' | Home' }
   end
 
   describe "Test page" do
     before { visit help_path }
 
-    it { should have_selector 'h1', text: 'Help' }
+    it { should have_selector 'h1',    text: 'Help' }
     it { should have_selector 'title', text: full_title("Help") }
   end
 
   describe "About Page" do
     before { visit about_path }
 
-    it { should have_selector 'h1', text: "About Us" }
+    it { should have_selector 'h1',    text: "About Us" }
     it { should have_selector 'title', text: full_title("About Us") }
   end
 
   describe "Contact Page" do
     before { visit contact_path }
 
-    it { should have_selector 'h1', text: "Contact Us" }
+    it { should have_selector 'h1',    text: "Contact Us" }
     it { should have_selector 'title', text: full_title("Contact Us")}
   end
 end
