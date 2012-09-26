5dfbbb92b4af9d06d7949c27c741e10b9aba84d6
jjbachiller <jjbachiller@gmail.com>
2012-09-26 20:00:16 +0200

    Routes friendly and old index.html in public folder deleted

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "StaticPages" do
   describe "Home page" do
 
   	it "should have the content 'Sample App'" do
-  		visit '/static_pages/home'
+  		visit root_path
   		page.should have_content('Sample App')
   	end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => title_start)
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => ' | Home')
     end
   end
@@ -25,36 +25,36 @@ describe "StaticPages" do
   describe "Test page" do
 
   	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_content('Help')
   	end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => title_start + " | Help")
     end
   end
 
   describe "About Page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => title_start + " | About Us")
     end    
   end
 
   describe "Contact Page" do
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_content('Contact Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', :text => title_start + " | Contact Us")
     end
   end
