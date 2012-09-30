68f645ddfe721ae8c5d0fe311e695233423af8b2
jjbachiller <jjbachiller@gmail.com>
2012-09-30 19:51:45 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: [:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
   	@user = User.new
   end
@@ -6,8 +14,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-      # Esta línea no hace falta y no se por que, pero al registrarse ya accede solo.
-      # sign_in @user
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else
@@ -18,4 +25,45 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def edit
+    # Ya no hace falta, ya que lo va a obtener el filtro: correct_user
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Your profile has been updated";
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  private 
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." 
+      end
+    end
+
+  private
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
+
+  private
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,7 @@ module SessionsHelper
 	def sign_in(user)
 		cookies.permanent[:remember_token] = user.remember_token
 		self.current_user = user
+		# self.current_user = User.find(2)
 	end
 
 	def sign_out
@@ -11,17 +12,31 @@ module SessionsHelper
 	end
 
 	def signed_in?
-		!current_user.nil?
+		# # FIXME!: Ojo esto está mal, por algún motivo al crear un nuevo objecto en el signup, se asigna al curren_user y peta si dejamos solo current_user.nil
+		!(current_user.nil?  || current_user.new_record?)
 	end
 
 	def current_user=(user)
-		@user = user
+		@current_user = user
 	end
 
 	def current_user
 		# ||= Solo hace la asignación, si @user no está definido. MOLA!
 		# Es una especie de operador ternario abreviado.
 		# x = x || "valor de x"  <==> x ||= "valor de x"
-		@user ||= User.find_by_remember_token(cookies[:remember_token])
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
+
+	def current_user?(user)
+		current_user == user
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.url
 	end
 end
